5bb65e608d8d75f016e4ca6e181a353c4e30fc41
Tech Youths <arsalanmohd237@gmail.com>
2021-05-02 14:15:57 +0530

    country.js and some styling

--- resources/js/countries.js
+++ resources/js/countries.js
@@ -204,3 +204,79 @@ let country_list = [
     { name: 'Yemen', code: 'YE' },
     { name: 'China', code: 'CN' }
 ];
+
+// Select search country elements
+const search_country_element = document.querySelector(".search-country");
+const country_list_element = document.querySelector(".country-list");
+const chang_country_btn = document.querySelector(".change-country");
+const close_list_btn = document.querySelector(".close");
+const input = document.getElementById('search-input');
+
+// Create country list
+function createCountryList(){
+    const num_countries = country_list.length;
+    let i=0, ul_list_id;
+    country_list.forEach( (country, index) =>{
+        if(index%Math.ceil(num_countries/num_of_ul_lists) == 0){
+            ul_list_id = `list-${i}`;
+            country_list_element.innerHTML += `<ul id='${ul_list_id}'></ul>`;
+            i++;
+        }
+
+        document.getElementById(`${ul_list_id}`).innerHTML += `
+           <li onclick="fetchData('${country.name}')" id="${country.name}">
+           ${country.name}
+           </li>
+        `;
+    })
+}
+
+let num_of_ul_lists = 3;
+createCountryList();
+
+// Show / Hide the country list on click event
+// Added on change button to show or hide the country list
+chang_country_btn.addEventListener("click", function(){
+    // Whenever we click on change button input text box will always be empty
+    input.value = "";
+    // Resets the country list
+    // It is necessary to reset country list because whenever we -
+    // -type in any country name in input box then we add or remove class hide
+    resetCountryList();
+    search_country_element.classList.toggle("hide");
+    search_country_element.classList.toggle("fadeIn");
+});
+
+// When click on close button I need to hide the country list
+close_list_btn.addEventListener("click", function(){
+    search_country_element.classList.toggle("hide");
+});
+
+country_list_element.addEventListener("click", function(){
+    search_country_element.classList.toggle("hide");
+});
+
+// Country filter to search country by typing its name in text box
+// Input event fires up only when value in it changes
+// We will loop and check if the given value is found then we will -
+// - remove hide class from it else we will add hide class in it
+input.addEventListener("input", function(){
+   let value = input.value.toUpperCase();
+   country_list.forEach(country =>{
+      if(country.name.toUpperCase().startsWith(value)){
+        document.getElementById(country.name).classList.remove("hide");
+      } else {
+        document.getElementById(country.name).classList.add("hide");
+      }
+   })
+})
+
+// Reset country list
+/* It is necessary to reset country inorder to show every country
+   in the country list. We have to do this because while typing
+   country name in input field we add or remove hide class from it.*/
+function resetCountryList(){
+    country_list.forEach( country => {
+        document.getElementById(country.name).classList.remove("hide");
+    })
+}   
